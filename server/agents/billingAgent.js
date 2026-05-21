9e9f2c4013f77f200b2ea661d0e0ba1fe59e4552
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-05-21 11:56:55 +0530

    Create billingAgent.js

---
+++ server/agents/billingAgent.js
@@ -0,0 +1,79 @@
+const plans = [
+
+  {
+
+    name:"Starter",
+
+    price:7,
+
+    currency:"USD",
+
+    ram:"1GB",
+
+    cpu:"1 vCPU",
+
+    storage:"10GB",
+
+    bandwidth:"100GB"
+
+  },
+
+  {
+
+    name:"Pro",
+
+    price:25,
+
+    currency:"USD",
+
+    ram:"4GB",
+
+    cpu:"2 vCPU",
+
+    storage:"50GB",
+
+    bandwidth:"500GB"
+
+  },
+
+  {
+
+    name:"Enterprise",
+
+    price:99,
+
+    currency:"USD",
+
+    ram:"16GB",
+
+    cpu:"8 vCPU",
+
+    storage:"200GB",
+
+    bandwidth:"Unlimited"
+
+  },
+
+  {
+
+    name:"Ultra Enterprise",
+
+    price:199,
+
+    currency:"USD",
+
+    ram:"64GB",
+
+    cpu:"16 vCPU",
+
+    storage:"1TB",
+
+    bandwidth:"Unlimited",
+
+    prioritySupport:true,
+
+    dedicatedInfrastructure:true
+
+  }
+
+];
