b20a5d3fc85773bb551ae6e03be99206683c4e5c
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-09-04 11:51:35 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,7 +37,7 @@ function setAuthCookie(res, token) {
   res.cookie("access_token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/"
   });
@@ -499,7 +499,7 @@ async function logoutUser(req, res) {
         secure:
           process.env.NODE_ENV ===
           "production",
-        sameSite: "lax",
+        sameSite: "none",
         path: "/"
       }
     );
